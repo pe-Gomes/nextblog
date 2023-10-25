bf72f62bb280fa1f9791be0a0a8c4ce0c64d9af0
Pedro Gomes <pedrocgomes@proton.me>
2023-10-25 11:44:18 -0400

    feat: add styled-components and theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from 'next/app'
 
+import { GlobalStyle } from '@/styles/globals'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '@/styles/theme'
+
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({ subsets: ['latin'], variable: '--font-nunito' })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+export const theme = {
+  COLORS: {
+    blue: '#3294f8',
+    base_title: '#E7edf4',
+    base_description: '#c4d4e3',
+    base_text: '#AFC2D4',
+    base_span: '#7B96B2',
+    base_label: '#3A536B',
+    base_border: '#1C2F41',
+    base_post: '#112131',
+    base_profile: '#0B1B2B',
+    base_background: '#071422',
+    base_input: '#040F1A',
+  },
+  FONTS: {
+    NUNITO: {
+      'font-family': 'var(--font-nunito)',
+      'line-height': '160%',
+    },
+    MONOSPACE: {
+      'font-family': 'monospace',
+      'line-height': '160%',
+    },
+  },
+}
